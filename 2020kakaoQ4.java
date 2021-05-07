9f2417bfbf11c840efb0a333a0b7478e9a714d00
Park Jongdae <79396535+DoMyBestFor@users.noreply.github.com>
2021-05-07 19:10:10 +0900

    Update 2020kakaoQ4.java

--- 2020kakaoQ4.java
+++ 2020kakaoQ4.java
@@ -1,89 +1,64 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {-1, +1, 0, 0}; // 상하좌우
-    int[] dy = {0, 0, -1, +1}; // 상하좌우
-    int[][] board;   
+    int[] dx = {-1, +1, 0, 0}; // 상, 하, 좌, 우
+    int[] dy = {0, 0, -1, +1};
+    int[][] board;
     
-    
-    int answer = Integer.MAX_VALUE;
-    
-    
-    class Pos{
+    class Rail{
         int x;
         int y;
         int dir;
         int cost;
-        public Pos(int x, int y, int dir, int cost){
+        public Rail(int x, int y, int dir, int cost){
             this.x = x;
             this.y = y;
             this.dir = dir;
             this.cost = cost;
         }
     }
+    
     public int solution(int[][] board) {
-        // N X N 크기의 정사각형 보드
-        // 0은 칸이 빔, 1은 벽으로 채워짐, 벽에는 도로 건설 불가
-        // 출발점은 0,0 도착점은 n-1, n-1
-        // 직선 도로 : 100원, 코너 : 500원
-        // 도로 건설 시 필요한 최소 비용
-        
-        // 처음 방향에서 방향이 달라질 때마다 코너 비용이 발생하겠구나
+        // 0은 비어있음, 1은 벽으로 채워져 있음
         this.board = board;
-        
-        bfs();
-        
-        return answer;
+        bfs(0, 0, -1);
+            
+        return this.board[board.length-1][board[0].length-1];
     }
     
-    public void bfs(){
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(new Pos(0, 0, -1, 0));
+    public void bfs(int x, int y, int dir){
+        Queue<Rail> queue = new LinkedList<>();
+        queue.offer(new Rail(x, y, dir, 0));
         
         while(!queue.isEmpty()){
-            Pos pos = queue.poll();
-    
-            if(pos.x == board.length-1 && pos.y == board.length-1){
-                answer = Math.min(answer, board[pos.x][pos.y]);
-                continue;
-            }
+            Rail rail = queue.poll();
             
-            // 사방향으로 움직이면서 비용 업데이트
             for(int i = 0; i < 4; i++){
-                int x = pos.x + dx[i];
-                int y = pos.y + dy[i];
+                int new_x = rail.x + dx[i];
+                int new_y = rail.y + dy[i];
                 
-                if(x < 0 || y < 0 || x >= board.length || y >= board.length){
-                    // 인덱스 초과 시에는
-                    continue;
-                }
-                
-                if(board[x][y] == 1){
-                    // 벽이면
-                    continue;
-                }
-                
-                int cost = 0;
-                if(pos.dir == i || pos.dir == -1){
-                    cost = pos.cost + 100;
-                }
-                // 방향이 다르면 코너 비용도 더해줌 600원
-                else if(pos.dir != i){
-                    cost = pos.cost + 600;
-                }
-                
-                if(board[x][y] == 0){
-                    board[x][y] = cost;
-                    queue.offer(new Pos(x, y, i, cost));
-                }
-                else if(board[x][y] >= cost){
-                    board[x][y] = cost;
-                    queue.offer(new Pos(x, y, i, cost));
-                }
+                if(new_x >= 0 && new_y >= 0 && new_x < board.length && new_y < board[0].length){
+                    // 인덱스를 벗어나지 않는다면
+                    if(board[new_x][new_y] == 1){
+                        continue;
+                    }
+                    int cost = 0;
+                    if(rail.dir == i || rail.dir == -1)
+                        cost = rail.cost + 100;
+                    else if(rail.dir != i){
+                        cost = rail.cost + 600;
+                    }
                     
-                
+                    if(board[new_x][new_y] == 0){
+                        board[new_x][new_y] = cost;
+                        queue.offer(new Rail(new_x, new_y, i, cost));
+                    }
+                    else if(board[new_x][new_y] >= cost){
+                        board[new_x][new_y] = cost;
+                        queue.offer(new Rail(new_x, new_y, i, cost));
+                    }
+                }
             }
         }
-        
     }
 }

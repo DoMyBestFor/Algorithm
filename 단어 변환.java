51bc53798d50842263ff61e04f5f4d2e4a0f5feb
Park Jongdae <79396535+DoMyBestFor@users.noreply.github.com>
2021-07-11 12:21:11 +0900

    Update 단어 변환.java

--- 단어 변환.java
+++ 단어 변환.java
@@ -1,39 +1,47 @@
 import java.util.*;
 
 class Solution {
-    int min = Integer.MAX_VALUE;
-    HashSet<String> visited;
+   int n;
+int min = Integer.MAX_VALUE;
+boolean[] visited;
     public int solution(String begin, String target, String[] words) {
-        // DFS 의심 
-        visited = new HashSet<>();
-        visited.add(begin);
-        DFS(begin, target, words, 0);
-        
+        n = begin.length();
+visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
         if(min == Integer.MAX_VALUE){
-            min = 0;
+            return 0;
         }
         return min;
+      
     }
+public void dfs(String begin, String target, String[] words, int count){
+if(begin.equals(target)){
+min = Math.min(count, min);
+    return;
+}
+
+for(int i = 0; i < words.length; i++){
+if(changable(begin, words[i])){
+if(!visited[i]){
+visited[i] = true;
+dfs(words[i], target, words, count + 1);
+visited[i] = false;
+}
+}
+}
+}
+
+    public boolean changable(String a, String b){
+	int result = 0;
+for(int i = 0; i < n; i++){
+	if(a.charAt(i) == b.charAt(i)){
+result++;
+}
+
+}
+if(result == n-1)
+return true;
+return false;
+}
     
-    public void DFS(String begin, String target, String[] words, int answer){
-        if(begin.equals(target)){
-            min = Math.min(min, answer);
-            return;
-        }
-        
-        for(String str : words){
-            int count = 0;
-            for(int i = 0; i < begin.length(); i++){
-                if(str.charAt(i) == begin.charAt(i))
-                    count++;
-            }
-            if(count == begin.length() - 1){
-                    if(visited.contains(str))
-                        continue;
-                    visited.add(str);
-                    DFS(str, target, words, answer + 1);
-                    visited.remove(str);
-                }
-        }
-    }
 }

2bbdf0382cde96c37f96da417ac6befe7549365b
Park Jongdae <asdfg9377@naver.com>
2021-08-01 00:01:23 +0900

    2206

--- publicsolve/src/baekjoon/B2206.java
+++ publicsolve/src/baekjoon/B2206.java
@@ -1,3 +1,4 @@
+package baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -8,105 +9,96 @@ import java.util.StringTokenizer;
 // 기본적인 BFS 이지만 벽을 부쉈는지 안 부쉈는지 경우의 수를 따로 생각해 주어야 한다.
 // 벽을 부쉈는지 안 부쉈는지 판단하기 위해 Pos 클래스에 broken 변수를 추가한다.
 
-public class Main{
-	private static int N, M;
-	private static int[][] map;
-	private static int[][] visited;
+public class B2206{
+	static int n, m;
+	static int answer = Integer.MAX_VALUE;
+	static int[][] arr;
+	static int[][] visited;
 	
-	private static int[] dx = {-1, 1, 0, 0}; // 좌, 우, 상, 하
-	private static int[] dy = {0, 0, 1, -1};
-	
-	private static int min = Integer.MAX_VALUE;
-	
-	static class Pos{
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static class Pos implements Comparable<Pos>{
 		int x;
 		int y;
-		int dis;
+		int dist;
 		int broken;
-		public Pos(int x, int y, int dis, int broken) {
+		public Pos(int x, int y, int dist, int broken) {
+			super();
 			this.x = x;
 			this.y = y;
-			this.dis = dis;
+			this.dist = dist;
 			this.broken = broken;
 		}
+		public int compareTo(Pos other) {
+			return Integer.compare(this.dist, other.dist);
+		}
+		
 	}
-	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N+1][M+1];
-		visited = new int[N+1][M+1];
-		for(int i = 1; i < N + 1; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n][m];
+		visited = new int[n][m];
+		for(int i = 0; i < n; i++) {
 			Arrays.fill(visited[i], Integer.MAX_VALUE);
 		}
-		// visited 배열의 값이 무한대 : 방문 안 함
-		// 0 : 방문 했는데 벽을 뚫진 않음
-		// 1 : 방문 했는데 벽을 뚫었음
-		
-		for(int i = 1; i < N+1; i++) {
+		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			String str = st.nextToken();
-			for(int j = 1; j < M+1; j++) {
-				map[i][j] = str.charAt(j-1) - '0';
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = str.charAt(j) - '0';
 			}
 		}
 		
-		bfs();
-		
-		if(min == Integer.MAX_VALUE) {
+		bfs(new Pos(0, 0, 1, 0));
+		if(answer == Integer.MAX_VALUE) {
 			System.out.println(-1);
 			return;
 		}
-		
-		System.out.println(min + 1);
-		
+		System.out.println(answer);
 	}
 	
-	public static void bfs() {
-		Queue<Pos> queue = new LinkedList<>();
-		queue.offer(new Pos(1, 1, 0, 0)); // 시작 포인트 넣기
-		visited[1][1] = 0;
+	public static void bfs(Pos p) {
+		Queue<Pos> q = new LinkedList<>();
+		q.offer(p);
+		visited[p.x][p.y] = 0;
 		
-		while(!queue.isEmpty()) {
-			Pos pos = queue.poll();
-			
-			if(pos.x == N && pos.y == M) {
-				min = Math.min(min, pos.dis);
+		while(!q.isEmpty()) {
+			Pos pos = q.poll();
+			if(pos.x == n - 1 && pos.y == m - 1) {
+				answer = pos.dist;
+				break;
 			}
 			
 			for(int i = 0; i < 4; i++) {
-				int new_x = pos.x + dx[i];
-				int new_y = pos.y + dy[i];
+				int nx = pos.x + dx[i];
+				int ny = pos.y + dy[i];
 				
-				if(new_x < 1 || new_x > N || new_y < 1 || new_y > M) {
+				if(nx < 0 || ny < 0 || nx >= n || ny >= m)
 					continue;
-				}
-				if(visited[new_x][new_y] <= pos.broken) {
-					// 방문했으면
-					continue;
-				}
 				
-				if(map[new_x][new_y] == 0) {
-					// 벽이 아니면
-					visited[new_x][new_y] = pos.broken;
-					queue.offer(new Pos(new_x, new_y, pos.dis+1, pos.broken));
-				}
-				else {
-					// 벽을 만났는데 벽을 파괴할 기회 한 번이 남아있는 경우
-					if(pos.broken == 0) {
-						visited[new_x][new_y] = pos.broken + 1;
-						queue.offer(new Pos(new_x, new_y, pos.dis+1, pos.broken+1));
+				if(visited[nx][ny] > pos.broken) {
+					// 방문 안했으면
+					if(arr[nx][ny] == 0) {
+						// 벽이 아니면 그냥 퍼트려
+						visited[nx][ny] = pos.broken;
+						q.offer(new Pos(nx, ny, pos.dist + 1, pos.broken));
+					}
+					else if(arr[nx][ny] == 1) {
+						// 벽이라면 부술 수 있는지 확인하고 부숴서 가보자
+						if(pos.broken == 0) {
+							visited[nx][ny] = 1;
+							q.offer(new Pos(nx, ny, pos.dist + 1, 1));
+						}
+						else {
+							continue;
+						}
 					}
 				}
-				
-				
 			}
 		}
-		
-		
 	}
 }

15e3c1fde67f972587c8e1bd11f768fb7d7aeda4
Park Jongdae <asdfg9377@naver.com>
2021-08-08 00:31:30 +0900

    SW 9280

--- publicsolve/src/swexpert/S9280.java
+++ publicsolve/src/swexpert/S9280.java
@@ -24,32 +24,40 @@ public class S9280 {
 			n = Integer.parseInt(st.nextToken()); // 주차 공간의 수
 			m = Integer.parseInt(st.nextToken()); // 오늘 들어올 차의 수
 			
-			int sum = 0;
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
-			Queue<Integer> queue = new LinkedList<>();
-			HashMap<Integer, Integer> map = new HashMap<>();
-			pee = new int[n + 1];
-			weight = new int[m + 1];
+			int sum = 0; // 벌어들일 돈
+			
+			PriorityQueue<Integer> pq = new PriorityQueue<>(); // 남아 있는 주차 공간을 저장할 거야. 근데 번호가 작은 순으로 저장
+			Queue<Integer> queue = new LinkedList<>(); // 대기중인 차를 저장할 것.
+			HashMap<Integer, Integer> map = new HashMap<>(); // 차 번호, 배치 주차 공간 을 저장할 것
+			
+			pee = new int[n + 1]; // 주차 공간에 따른 비용
+			weight = new int[m + 1]; // 차의 무게
+			
 			for(int i = 1; i <= n; i++) {
-				pq.offer(i);
+				pq.offer(i); // 처음에는 주차공간 다 비어있음
 			}
+			
 			for(int i = 1; i <= n; i++) {
 				// 주차 공간당 단위 무게 요금을 저장할 거야.
 				st = new StringTokenizer(br.readLine());
 				pee[i] = Integer.parseInt(st.nextToken());
 			}
+			
 			for(int i = 1; i <= m; i++) {
 				// 각 차의 무게를 저장할거야
 				st = new StringTokenizer(br.readLine());
 				weight[i] = Integer.parseInt(st.nextToken());
 			}
+			
 			for(int i = 0; i < 2 * m; i++) {
 				st = new StringTokenizer(br.readLine());
 				int command = Integer.parseInt(st.nextToken());
+				
+				// 양수이면 들어가는 것
 				if(command > 0) {
 					if(pq.isEmpty()) {
 						// 주차공간이 꽉 차 있으면
-						queue.offer(command); // 대기 큐에 차를 넣는다.
+						queue.offer(command); // 대기 큐에 차를 넣어 기다리게 한다.
 						continue;
 					}
 					// 주차 공간이 비어 있으면
@@ -57,6 +65,7 @@ public class S9280 {
 					sum += pee[idx] * weight[command]; // 주차 가격 책정
 					map.put(command, idx); // 넣는다
 				}
+				// 음수이면 나오는 것
 				else {
 					command = Math.abs(command); // 차 번호 얻기
 					pq.offer(map.get(command)); // 빈 공간을 갱신해준다.
@@ -70,6 +79,7 @@ public class S9280 {
 					}
 				}
 			}
+			
 			System.out.println("#" + tc + " " + sum);
 		}
 	}

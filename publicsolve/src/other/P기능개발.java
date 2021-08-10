4b72155bc36da5d07da56bf60e8d31cd544eadd8
Park Jongdae <asdfg9377@naver.com>
2021-08-10 22:26:25 +0900

    프로그래머스 기능개발

---
+++ publicsolve/src/other/P기능개발.java
@@ -0,0 +1,37 @@
+package other;
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<int[]> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < progresses.length; i++){
+            queue.offer(new int[] {progresses[i], i});
+        }
+        while(!queue.isEmpty()){
+            int count = 0;
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int[] tp = queue.poll();
+                queue.offer(new int[] {tp[0] + speeds[tp[1]], tp[1]});
+            }
+            while(!queue.isEmpty() && queue.peek()[0] >= 100){
+                count++;
+                queue.poll();
+            }
+            if(count != 0){
+                answer.add(count);
+            }
+        }
+        
+        int[] result = new int[answer.size()];
+        for(int i = 0; i < answer.size(); i++){
+            result[i] = answer.get(i);
+        }        
+        return result;
+        
+        
+        
+    }
+}

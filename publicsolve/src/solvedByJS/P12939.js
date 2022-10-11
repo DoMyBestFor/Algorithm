99277db20a9ff2e117d878231c63fb2e2cac89c3
Park Jongdae <asdfg9377@naver.com>
2022-10-11 23:19:38 +0900

    comment 수정

--- publicsolve/src/solvedByJS/P12939.js
+++ publicsolve/src/solvedByJS/P12939.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    // 공백으로 구분된 숫자, 최대와 최소를 찾자
-    const newS = s.split(' ').map((str) => Number(str));
-    return `${Math.min(...newS)} ${Math.max(...newS)}`;
+  const newS = s.split(" ").map((str) => Number(str));
+  return `${Math.min(...newS)} ${Math.max(...newS)}`;
 }
